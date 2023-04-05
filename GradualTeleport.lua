1ef3aff87f6d9c97286fbad46425014459cd3fcb
Kaiden00 <91414392+Kaiden00@users.noreply.github.com>
2023-04-05 11:37:57 +0100

    Update GradualTeleport.lua

--- GradualTeleport.lua
+++ GradualTeleport.lua
@@ -21,45 +21,40 @@ local function ImprovedTeleport(Target)
 
     local StartingPosition = HRP.Position;
     local PositionDelta = (Target - StartingPosition);
-    local StartTime = tick();
-    local TotalDuration = (StartingPosition - Target).magnitude / Teleport.TeleportSpeed;
+    local TotalDistance = PositionDelta.magnitude;
+    local TotalDuration = TotalDistance / Teleport.TeleportSpeed;
+
+    local RaycastParams = RaycastParams.new();
+    RaycastParams.FilterType = Enum.RaycastFilterType.Blacklist;
+    RaycastParams.IgnoreWater = true;
+    RaycastParams.CollisionGroup = "Default";
 
     _G.TeleportCount += 1
-    OldTeleportCount = _G.TeleportCount
-
-    local lastProgress = 0
-
-    local raycastParams = RaycastParams.new()
-    raycastParams.FilterDescendantsInstances = {Player.Character}
-    raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
+    local OldTeleportCount = _G.TeleportCount
     
-    repeat NextFrame:Wait();
+    repeat 
         local Delta = tick() - StartTime;
         local Progress = math.min(Delta / TotalDuration, 1);
         local MappedPosition = StartingPosition + (PositionDelta * Progress);
-        HRP.Velocity = Vector3.new();
-
-        -- Check if the player is about to go into the air and adjust the target position
-        local raycastResult = workspace:Raycast(MappedPosition, Vector3.new(0, -5, 0), raycastParams)
-        if raycastResult and raycastResult.Position.Y > MappedPosition.Y - 1.5 then
-            -- If there's an object in the way, move the target position down
-            Target = MappedPosition - Vector3.new(0, raycastResult.Position.Y - MappedPosition.Y + 1.5, 0)
-            PositionDelta = (Target - StartingPosition)
-            Progress = lastProgress -- Restore the last progress value to avoid jumping
-        else
-            -- Otherwise, update the last progress value
-            lastProgress = Progress
+        
+        local RaycastResult = workspace:Raycast(HRP.Position, MappedPosition - HRP.Position, RaycastParams);
+        if RaycastResult then
+            MappedPosition = RaycastResult.Position;
+            PositionDelta = (Target - MappedPosition);
+            TotalDistance = PositionDelta.magnitude;
+            TotalDuration = TotalDistance / Teleport.TeleportSpeed;
         end
 
+        HRP.Velocity = Vector3.new();
         HRP.CFrame = CFrame.new(MappedPosition);
-    until (HRP.Position - Target).magnitude <= Teleport.TeleportSpeed / 2 or OldTeleportCount ~= _G.TeleportCount;
-
+        NextFrame:Wait();
+    until (MappedPosition - Target).magnitude <= Teleport.TeleportSpeed / 2 or OldTeleportCount ~= _G.TeleportCount;
+    
     HRP.Anchored = false;
-
+    
     if OldTeleportCount ~= _G.TeleportCount then return end
     HRP.CFrame = CFrame.new(Target);
 end
-
 
 Teleport.TeleportTo = ImprovedTeleport
 
