13bd324e7dc1948862df2d26678d70c18c455af1
Kaiden00 <91414392+Kaiden00@users.noreply.github.com>
2023-04-05 11:10:00 +0100

    Update GradualTeleport.lua

--- GradualTeleport.lua
+++ GradualTeleport.lua
@@ -1,9 +1,13 @@
 --Credits to Nahida
+
+local Teleport = {
+    TeleportSpeed = 50
+}
+
 local RunService = game:GetService("RunService");
 local Players = game:GetService("Players");
 local Player = Players.LocalPlayer;
 
-local TeleportSpeed = 50;
 local NextFrame = RunService.Heartbeat;
 
 local function ImprovedTeleport(Target)
@@ -16,7 +20,7 @@ local function ImprovedTeleport(Target)
     local StartingPosition = HRP.Position;
     local PositionDelta = (Target - StartingPosition);--Calculating the difference between the start and end positions.
     local StartTime = tick();
-    local TotalDuration = (StartingPosition - Target).magnitude / TeleportSpeed;
+    local TotalDuration = (StartingPosition - Target).magnitude / Teleport.TeleportSpeed;
 
     repeat NextFrame:Wait();
         local Delta = tick() - StartTime;
@@ -25,9 +29,11 @@ local function ImprovedTeleport(Target)
         local MappedPosition = StartingPosition + (PositionDelta * Progress);
         HRP.Velocity = Vector3.new();--Resetting the effect of gravity so it doesn't get too much and drag the player below the ground.
         HRP.CFrame = CFrame.new(MappedPosition);
-    until (HRP.Position - Target).magnitude <= TeleportSpeed / 2;
+    until (HRP.Position - Target).magnitude <= Teleport.TeleportSpeed / 2;
     HRP.Anchored = false;
     HRP.CFrame = CFrame.new(Target);
 end
 
-return ImprovedTeleport
+Teleport.TeleportTo = ImprovedTeleport
+
+return Teleport
