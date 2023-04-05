59fb2ca6d7e09dc7d317a36c21cf14b50f5f7c23
Kaiden00 <91414392+Kaiden00@users.noreply.github.com>
2023-04-05 11:17:04 +0100

    Update GradualTeleport.lua

--- GradualTeleport.lua
+++ GradualTeleport.lua
@@ -10,6 +10,8 @@ local Player = Players.LocalPlayer;
 
 local NextFrame = RunService.Heartbeat;
 
+_G.TeleportCount = 0
+
 local function ImprovedTeleport(Target)
     if (typeof(Target) == "Instance" and Target:IsA("BasePart")) then Target = Target.Position; end;
     if (typeof(Target) == "CFrame") then Target = Target.p end;
@@ -21,7 +23,10 @@ local function ImprovedTeleport(Target)
     local PositionDelta = (Target - StartingPosition);--Calculating the difference between the start and end positions.
     local StartTime = tick();
     local TotalDuration = (StartingPosition - Target).magnitude / Teleport.TeleportSpeed;
-
+    
+    _G.TeleportCount += 1
+    OldTeleportCount = _G.TeleportCount
+    
     repeat NextFrame:Wait();
         local Delta = tick() - StartTime;
         local Progress = math.min(Delta / TotalDuration, 1);--Getting the percentage of completion of the teleport (between 0-1, not 0-100)
@@ -29,8 +34,11 @@ local function ImprovedTeleport(Target)
         local MappedPosition = StartingPosition + (PositionDelta * Progress);
         HRP.Velocity = Vector3.new();--Resetting the effect of gravity so it doesn't get too much and drag the player below the ground.
         HRP.CFrame = CFrame.new(MappedPosition);
-    until (HRP.Position - Target).magnitude <= Teleport.TeleportSpeed / 2;
+    until (HRP.Position - Target).magnitude <= Teleport.TeleportSpeed / 2 or _G.TeleportCount ~= OldTeleportCount;
+    
     HRP.Anchored = false;
+    
+    if _G.TeleportCount ~= OldTeleportCount then return end --if we override the teleport to another destination we may not actually be near the target which could kick us
     HRP.CFrame = CFrame.new(Target);
 end
 
