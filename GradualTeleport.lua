7404ee7405e84fbe69c336113f790e98eea6125f
Kaiden00 <91414392+Kaiden00@users.noreply.github.com>
2023-04-08 15:41:27 +0100

    Update GradualTeleport.lua

--- GradualTeleport.lua
+++ GradualTeleport.lua
@@ -1,16 +1,11 @@
 --Credits to Nahida
 
-local Teleport = {
-    TeleportSpeed = 50
-}
-
 local RunService = game:GetService("RunService");
 local Players = game:GetService("Players");
 local Player = Players.LocalPlayer;
 
+local TeleportSpeed = 50;
 local NextFrame = RunService.Heartbeat;
-
-_G.TeleportCount = 0
 
 local function ImprovedTeleport(Target)
     if (typeof(Target) == "Instance" and Target:IsA("BasePart")) then Target = Target.Position; end;
@@ -22,11 +17,8 @@ local function ImprovedTeleport(Target)
     local StartingPosition = HRP.Position;
     local PositionDelta = (Target - StartingPosition);--Calculating the difference between the start and end positions.
     local StartTime = tick();
-    local TotalDuration = (StartingPosition - Target).magnitude / Teleport.TeleportSpeed;
-    
-    _G.TeleportCount += 1
-    OldTeleportCount = _G.TeleportCount
-    
+    local TotalDuration = (StartingPosition - Target).magnitude / TeleportSpeed;
+
     repeat NextFrame:Wait();
         local Delta = tick() - StartTime;
         local Progress = math.min(Delta / TotalDuration, 1);--Getting the percentage of completion of the teleport (between 0-1, not 0-100)
@@ -34,14 +26,7 @@ local function ImprovedTeleport(Target)
         local MappedPosition = StartingPosition + (PositionDelta * Progress);
         HRP.Velocity = Vector3.new();--Resetting the effect of gravity so it doesn't get too much and drag the player below the ground.
         HRP.CFrame = CFrame.new(MappedPosition);
-    until (HRP.Position - Target).magnitude <= Teleport.TeleportSpeed / 2 or OldTeleportCount ~= _G.TeleportCount;
-    
+    until (HRP.Position - Target).magnitude <= TeleportSpeed / 2;
     HRP.Anchored = false;
-    
-    if OldTeleportCount ~= _G.TeleportCount then return end --if we override the teleport to another destination we may not actually be near the target which could kick us
     HRP.CFrame = CFrame.new(Target);
-end
-
-Teleport.TeleportTo = ImprovedTeleport
-
-return Teleport
+end;
